767b4a97278c3d75895a2402b0082e0f16cb273b
NaveenChouhan <nk4447929@gmail.com>
2025-05-08 12:08:28 +0530

    Update aryascript.js

---
+++ aryascript.js
@@ -0,0 +1,7 @@
+let readmorebtn=document.querySelector(".principalmorebtn");
+let readmore=document.querySelector(".principalmore");
+
+readmorebtn.addEventListener("click", function(){
+    readmore.style.display="block";
+    readmorebtn.style.display="none";
+})
